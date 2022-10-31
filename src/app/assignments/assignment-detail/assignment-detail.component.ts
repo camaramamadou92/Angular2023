bfa71f2a1953e4862738b5474b969f4f70c35605
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2022-10-31 14:27:03 +0100

    correctif

--- src/app/assignments/assignment-detail/assignment-detail.component.ts
+++ src/app/assignments/assignment-detail/assignment-detail.component.ts
@@ -1,20 +1,44 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AssignmentsService } from 'src/app/shared/assignments.service';
 import { Assignment } from '../assignment.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-assignment-detail',
   templateUrl: './assignment-detail.component.html',
-  styleUrls: ['./assignment-detail.component.css']
+  styleUrls: ['./assignment-detail.component.css'],
 })
 export class AssignmentDetailComponent implements OnInit {
-  @Input() assignmentTransmis!:Assignment;
+  /*@Input() */ assignmentTransmis!: Assignment|undefined ;
 
-  constructor() { }
+  constructor(private assignmentsService: AssignmentsService, private route:ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.getAssignment();
+  }
+  getAssignment(){
+    //on recupere  l'id dans le snapshot passé par le routeur
+    //le "+"  force la conversion  de l'id de type string en "number"
+    const id = +this.route.snapshot.params['id'];
+    this.assignmentsService.getAssignment(id)
+       .subscribe(a => this.assignmentTransmis = a);
   }
 
   onAssignmentRendu() {
+    if (!this.assignmentTransmis) return;
     this.assignmentTransmis.rendu = true;
+    this.assignmentsService
+      .updateAssignment(this.assignmentTransmis)
+      .subscribe((message) => console.log(message));
+  }
+
+  onDeleteAssignment() {
+    if (!this.assignmentTransmis) return;
+    this.assignmentsService
+      .deleteAssignment(this.assignmentTransmis)
+      .subscribe((message) => {
+        console.log(message);
+        this.assignmentTransmis = undefined;
+      });
   }
 }
---
+++ src/app/shared/assignments.service.ts
@@ -0,0 +1,69 @@
+import { Injectable, ɵɵNgOnChangesFeature } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Assignment } from '../assignments/assignment.model';
+import { LoggingService } from './logging.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AssignmentsService {
+  assignments:Assignment[] = [
+    {
+      id: 1,
+      nom: 'Devoir Angular de Mr Buffa',
+      dateDeRendu: new Date('2022-11-30'),
+      rendu:false
+    },
+    {
+      id: 2,
+      nom: 'Devoir WebComponents de Mr Buffa',
+      dateDeRendu: new Date('2022-09-30'),
+      rendu:false
+    },
+    {
+      id:3,
+      nom: 'Devoir BD de Mr Mopolo',
+      dateDeRendu: new Date('2022-09-30'),
+      rendu:true
+    }
+  ]
+  constructor(private loggingService:LoggingService) { }
+
+  getAssignments():Observable<Assignment[]> {
+    return of(this.assignments);
+  }
+  getAssignment(id:number):Observable<Assignment|undefined>{
+    const a:Assignment|undefined =  this.assignments.find(a => a.id === id);
+    return of(a);
+  }
+
+  addAssignment(assignment:Assignment):Observable<string> {
+    this.assignments.push(assignment);
+    // ex utilisation du service de log
+    this.loggingService.log(assignment.nom, "ajouté");
+
+    return of("Assignment ajouté");
+  }
+
+  deleteAssignment(assignment:Assignment):Observable<string> {
+    const position = this.assignments.indexOf(assignment);
+    this.assignments.splice(position,1);
+
+      // ex utilisation du service de log
+      this.loggingService.log(assignment.nom, "supprimé");
+
+    return of("Assignment supprimé");
+
+  }
+
+  updateAssignment(assignment:Assignment):Observable<string> {
+    // Rien à faire pour le moment, plus tard
+    // il faudra faire une requête HTTP PUT
+    // sur un web service distant etc.
+
+      // ex utilisation du service de log
+      this.loggingService.log(assignment.nom, "modifié");
+
+    return of("Assignment modifié");
+  }
+}
